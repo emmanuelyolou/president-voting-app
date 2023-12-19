08c39012e17008101df337890eca77ad14ac2f05
Emmanuel Yolou <emmanuelyolou@gmail.com>
2023-12-19 12:05:07 +0000

    Add of the nextui responsive navbar

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,75 +17,75 @@ export default function Header() {
   
   return (
     <header className=''>
-        <Navbar
+    <Navbar
         maxWidth='full'
       isBordered
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
     >
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-      </NavbarContent>
+        <NavbarContent className="sm:hidden" justify="start">
+            <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+        </NavbarContent>
 
-      <NavbarContent className="sm:hidden pr-3" justify="start">
-        <NavbarBrand>
-          {/* <AcmeLogo /> */}
-          <p className="font-bold text-inherit">Election IDSI</p>
-        </NavbarBrand>
-      </NavbarContent>
+        <NavbarContent className="sm:hidden pr-3" justify="start">
+            <NavbarBrand>
+            {/* <AcmeLogo /> */}
+            <p className="font-bold text-inherit">Election IDSI</p>
+            </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="start">
-        <NavbarBrand>
-          {/* <AcmeLogo /> */}
-          <p className="font-bold text-inherit">Election IDSI</p>
-        </NavbarBrand>
-      </NavbarContent>
+        <NavbarContent className="hidden sm:flex gap-4" justify="start">
+            <NavbarBrand>
+            {/* <AcmeLogo /> */}
+            <p className="font-bold text-inherit">Election IDSI</p>
+            </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent>
-        <NavbarItem>
-            <Link color="foreground" href="#">
-                Candidats
-            </Link>
-            </NavbarItem>
+        <NavbarContent className='max-sm:hidden'>
             <NavbarItem>
-            <Link href="#" aria-current="page">
-                Comité d&apos;Organisation
-            </Link>
+                <Link color="foreground" href="#">
+                    Candidats
+                </Link>
+                </NavbarItem>
+                <NavbarItem>
+                <Link href="#" aria-current="page">
+                    Comité d&apos;Organisation
+                </Link>
+                </NavbarItem>
+                <NavbarItem>
+                <Link color="foreground" href="#">
+                    Résultats
+                </Link>
             </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent justify="end">
+            {/* <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+            </NavbarItem> */}
             <NavbarItem>
-            <Link color="foreground" href="#">
-                Résultats
-            </Link>
-        </NavbarItem>
-      </NavbarContent>
+            <Button as={Link} color="primary" href="login" variant="flat">
+                Se connecter
+            </Button>
+            </NavbarItem>
+        </NavbarContent>
 
-      <NavbarContent justify="end">
-        {/* <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem> */}
-        <NavbarItem>
-          <Button as={Link} color="primary" href="login" variant="flat">
-            Se connecter
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
+        <NavbarMenu>
+            {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+                <Link
+                className="w-full"
+                color={
+                    index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                }
+                href="#"
+                size="lg"
+                >
+                {item}
+                </Link>
+            </NavbarMenuItem>
+            ))}
+        </NavbarMenu>
     </Navbar>
     </header>
   )
