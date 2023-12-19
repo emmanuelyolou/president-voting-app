93f259023503561db2ab3c6affea68e5f38ede0c
Emmanuel Yolou <emmanuelyolou@gmail.com>
2023-12-19 17:18:22 +0000

    Merge completed with the origin

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,31 +26,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
-import Link from "next/link";
-import Header from "./components/Header";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "President election",
-  description: "Voting the Idsi President",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="fr">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
-    </html>
-  )
-}
