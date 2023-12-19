c903f0d52452a4412097d009131757d9125b4635
Emmanuel Yolou <emmanuelyolou@gmail.com>
2023-12-19 18:20:26 +0000

    r

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Stats() {
   return (
     <main>
-        <div className="grid md:grid-cols-4">
+        <div className="grid md:grid-cols-2 container mx-auto mt-8 gap-x-8 gap-y-16">
+            <div className="rounded-lg shadow-md p-8 flex flex-col items-center">
+              <div className="">
+                <span className="text-6xl">37</span>
+              </div>
+              Votants
+            </div>
+
             
         </div>
     </main>
